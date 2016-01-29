a3f3c1522d1131b0576555b3ff1ab466d0e5968c
Blake Johnson <blake41@gmail.com>
2016-01-29 14:25:06 -0500

    fixed specs

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -9,6 +9,7 @@ module Features
     end
 
     def signin(email, password)
+      # debugger
       visit new_user_session_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
