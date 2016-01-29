68e0ac29e177654c34de0c71a93d6fb1f4b820f7
Blake Johnson <blake41@gmail.com>
2016-01-29 14:59:59 -0500

    passing sign in sign out user delete user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable, :confirmable,
-  #        :recoverable, :rememberable, :trackable, :validatable
+
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :confirmable,
+         # :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable
+
   has_many :notes
   has_many :viewers
   has_many :readable, through: :viewers, source: :note
