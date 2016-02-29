d9ba3dfd3a15b82fc3f7f708b8d69475c0142a4c
Charles Pletcher <cpletcher2@gmail.com>
2016-02-29 09:59:19 -0500

    Convert lab to sqlite3, add skeleton for policies
    Fixes #6
    Obsoletes #8
    Fixes #11
    Fixes #15
    
    This commit adds empty ApplicationPolicy and UserPolicy classes so that tests can run.
    This commit also removes dependencies on postgres and instead sets users up for sqlite3.
    
    There were some postgres-specific features (namely, foreign key constraints and inet columns) in some migrations, but everything seems to be running smoothly.
    
    Remove unneeded file

--- db/migrate/20151213020140_create_notes.rb
+++ db/migrate/20151213020140_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
       t.string :content
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true
 
       t.timestamps null: false
     end
